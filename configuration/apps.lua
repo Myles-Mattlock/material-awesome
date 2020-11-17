4559f834d38fe853ec59cef38d27d7beda283a82
Myles-Mattlock <65444145+Myles-Mattlock@users.noreply.github.com>
2020-11-17 21:08:01 +0000

    Update apps.lua

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,36 +8,38 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'terminator',
+    terminal = 'konsole',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'terminator',
+    quake = 'konsole',
     screenshot = 'flameshot full -p ~/Pictures',
     region_screenshot = 'flameshot gui -p ~/Pictures',
     delayed_screenshot = 'flameshot full -p ~/Pictures -d 5000',
-    browser = 'brave',
-    editor = 'gedit', -- gui text editor
+    browser = 'brave-browser',
+    editor = 'geany', -- gui text editor
     social = 'discord',
     game = rofi_command,
-    files = 'nautilus',
+    files = 'nemo',
     music = rofi_command 
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
-    'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
+   -- 'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
     'nm-applet --indicator', -- wifi
     'pnmixer', -- shows an audiocontrol applet in systray when installed.
-    --'blueberry-tray', -- Bluetooth tray icon
-    'numlockx on', -- enable numlock
-    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    'blueberry-tray', -- Bluetooth tray icon
+    --'numlockx on', -- enable numlock
+    '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
      'flameshot',
-     'synology-drive -minimized',
-     'steam -silent',
-     '/usr/bin/barrier',
-     '~/.local/bin/wallpaper', -- wallpaper-reddit script
+    -- 'synology-drive -minimized',
+     --'steam -silent',
+      --'~/.local/bin/wallpaper', -- wallpaper-reddit script
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
-    '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+    '~/.config/awesome/configuration/awspawn', -- Spawn "dirty" apps that can linger between sessions
+    'picom --config ~/.config/picom/picom.conf',
+    'mintupdate-launcher',
+    'xinput set-prop "SYNA2B38:00 06CB:7FB5 Touchpad" "libinput Tapping Enabled" 1'    
   }
 }
